ca267fbbe98eeae8f5048c3ce87a4fcca2f06cac
eastern-winds <ecorbin04@proton.me>
2026-04-13 18:00:51 -0400

    Create program5.js

---
+++ program5.js
@@ -0,0 +1,25 @@
+async function displayTable(selectedMajor) {
+    var responce = await fetch("cit5students.json");
+
+    if(responce.ok) {
+        var templateScript = document.getElementById("tblTemplate");
+
+        var template = Handlebars.compile(templateScript);
+
+        var context = responce.filter(function(stu, selectedMajor) {
+            return stu.major == selectedMajor;
+        });
+
+        var compTemplate = template({student : context});
+
+        document.getElementById("out").innerHTML = compTemplate;
+    }
+    else {
+        document.getElementById("out").innerHTML = "Error in fetthing 'cit5students.json'."
+    }
+}
+
+window.onload = function () {
+    document.getElementById("busmajor").onclick = function() { displayTable("BUS") };
+    document.getElementById("citmajor").onclick = function() { displayTable("CIT") };
+}
